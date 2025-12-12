12755de8a019298fea38869e73769e04e86c663d
Marcos Aleixo <maknytz@gmail.com>
2025-12-12 15:16:24 -0300

    Ajustando a conexão

--- conexao.php
+++ conexao.php
@@ -1,11 +1,11 @@
 <?php
 
 $host = "localhost";
-$db = "select";
 $user = "php";
 $pass = 123456;
+$db = "select";
 
-$conn = new mysqli($host, $$user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_errno) {
     die("Falha na conexão! (" . $conn->connect_errno . ") " . $conn->connect_error);
